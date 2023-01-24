64b37cc3a76fbf0247b629f723ece66563efad08
james4-daf <d.james@elsevier.com>
2023-01-24 18:28:21 +0000

    iter 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "bulma/css/bulma.css";
 import "./App.css";
+import FormField from "./components/FormField";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
     </div>
   );
 }
