5397edd64eebb7734c467843b12032a384871677
james4-daf <d.james@elsevier.com>
2023-01-24 18:19:48 +0000

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import "bulma/css/bulma.css";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
