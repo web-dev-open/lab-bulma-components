eca4e174bb16e8637a9115bca5a3e032d04c97ca
hackwell <hackwell.4216@gmail.com>
2023-11-11 19:54:24 +0000

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import "bulma/css/bulma.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Navbar />
     </div>
   );
 }
