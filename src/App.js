4925c22d6688655931855d50d116ff9a7be5ab56
hackwell <hackwell.4216@gmail.com>
2023-11-13 22:47:49 +0000

    complete iteration 1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
       <Navbar />
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,91 +28,19 @@ function Navbar() {
           <a class="navbar-item" href="https://bulma.io/">
             Home
           </a>
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a
-              class="navbar-link"
-              href="https://bulma.io/documentation/overview/start/"
-            >
-              Docs
-            </a>
-            <div class="navbar-dropdown is-boxed">
-              <a
-                class="navbar-item"
-                href="https://bulma.io/documentation/overview/start/"
-              >
-                Overview
-              </a>
-              <a
-                class="navbar-item"
-                href="https://bulma.io/documentation/overview/modifiers/"
-              >
-                Modifiers
-              </a>
-              <a
-                class="navbar-item"
-                href="https://bulma.io/documentation/columns/basics/"
-              >
-                Columns
-              </a>
-              <a
-                class="navbar-item"
-                href="https://bulma.io/documentation/layout/container/"
-              >
-                Layout
-              </a>
-              <a
-                class="navbar-item"
-                href="https://bulma.io/documentation/form/general/"
-              >
-                Form
-              </a>
-              <hr class="navbar-divider" />
-              <a
-                class="navbar-item"
-                href="https://bulma.io/documentation/elements/box/"
-              >
-                Elements
-              </a>
-              <a
-                class="navbar-item is-active"
-                href="https://bulma.io/documentation/components/breadcrumb/"
-              >
-                Components
-              </a>
-            </div>
-          </div>
         </div>
 
         <div class="navbar-end">
           <div class="navbar-item">
-            <div class="field is-grouped">
-              <p class="control">
-                <a
-                  class="bd-tw-button button"
-                  data-social-network="Twitter"
-                  data-social-action="tweet"
-                  data-social-target="https://bulma.io"
-                  target="_blank"
-                  href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms"
-                >
-                  <span class="icon">
-                    <i class="fab fa-twitter"></i>
-                  </span>
-                  <span>Tweet</span>
-                </a>
-              </p>
-              <p class="control">
-                <a
-                  class="button is-primary"
-                  href="https://github.com/jgthms/bulma/releases/download/0.9.4/bulma-0.9.4.zip"
-                >
-                  <span class="icon">
-                    <i class="fas fa-download"></i>
-                  </span>
-                  <span>Download</span>
-                </a>
-              </p>
-            </div>
+            <button
+              style={{ marginRight: "10px" }}
+              class="has-background-link has-text-white px-5 py-2 button is-link"
+            >
+              Login
+            </button>
+            <button class=" button has-background-primary has-text-white px-5 py-2 is-link">
+              Signup
+            </button>
           </div>
         </div>
       </div>
