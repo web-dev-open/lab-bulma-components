76fd919c5b4c8faa9c0c0aa8d5daba4cc241937d
PriyankaDandupati23 <priyadandu10@gmail.com>
2023-12-09 23:56:08 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import  "bulma/css/bulma.css";
+
+
+import SignupForm from "./components/Signupform.js";
+import CoolButton from "./components/CoolButton.js";
+import Message from "./components/Message.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SignupForm />
+
+      <CoolButton isSuccess>Button 1</CoolButton>
+      <CoolButton> Button 2 </CoolButton>
+      <CoolButton  isSmall  isDanger  isRounded>Button 1</CoolButton>
+      <CoolButton  isSmall  isSuccess>Button 2</CoolButton>
+
+      <Message  isInfo  title="Hello World">
+	      Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+	      <strong>Pellentesque risus mi</strong>.
+      </Message>
     </div>
   );
 }
