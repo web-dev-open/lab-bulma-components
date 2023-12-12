8e0957397bdd4b3c0fd5b7423f2e49619d122d38
M Sami <beboolean@gmail.com>
2023-12-12 14:52:57 +0530

    doen

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+import "bulma/css/bulma.css"
+import Message from './components/Message';
+
+import SignupForm from './components/SignupForm';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <SignupForm/>
+     <Message isInfo title="New Message">Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+	    <strong>Pellentesque risus mi</strong>.</Message>
+  
     </div>
   );
 }
