8e3bd003fd4d283b010110305a3ae941cf49ea6a
rajnandni123 <150055291+rajnandni123@users.noreply.github.com>
2023-12-14 00:09:45 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bulma/css/bulma.css";
+import Navbar from "./component/Navbar";
+import FormField from "./component/FormField";
+import SignupForm from "./component/SignupForm";
+import CoolButton from "./component/CoolButton";
+import Message from "./component/Message";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+
+      <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
+      <FormField
+        label="Email"
+        type="email"
+        placeholder="e.g. alexsmith@gmail.com"
+      />
+      <SignupForm />
+      <div style={{ display: "flex", justifyContent: "center", gap: "10px" }}>
+        <CoolButton isSuccess>Button 1</CoolButton>
+        <CoolButton> Button 2 </CoolButton>
+      </div>
+
+      {/* BONUS 1 */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: "10px",
+          margin: "10px",
+        }}
+      >
+        <CoolButton isSmall isDanger isRounded>
+          Button 1
+        </CoolButton>
+        <CoolButton isSmall isSuccess>
+          Button 2
+        </CoolButton>
+      </div>
+
+
+      {/* BONUS 2 */}
+      <Message isInfo title="Hello World">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        <strong>Pellentesque risus mi</strong>.
+      </Message>
     </div>
   );
 }
