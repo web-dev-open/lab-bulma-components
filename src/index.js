998580691ce319a39b95063084a3b37980dd844a
Devarsya shah <96256016+DEVARSYASHAH@users.noreply.github.com>
2023-12-14 23:21:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,6 +10,22 @@ root.render(
     <App />
   </React.StrictMode>
 );
+import React from 'react';
+import SignupForm from './components/SignupForm';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Other components or content goes here */}
+      
+      {/* Render the SignupForm component */}
+      <SignupForm />
+    </div>
+  );
+}
+
+export default App;
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
