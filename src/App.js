6b3176fc46c66ab861614ddd965e2cea36002977
jaysonalfie <jaysonalfiea20@students.tukenya.ac.ke>
2024-01-08 18:47:23 +0300

    Jayson Internship

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "bulma/css/bulma.css";
 import FormField from './components/FormField';
 import SignupForm from './components/SignupForm';
 import CoolButton from './components/CoolButton';
+import Message from './components/Message';
 
 function App() {
   return (
@@ -14,8 +15,16 @@ function App() {
       <FormField label="Name" type="text" placeholder="e.g Alex Smith" />
       <FormField label="Email" type="email" placeholder="e.g. alexsmith@gmail.com" />
       <SignupForm/>
-      <CoolButton isSuccess>Button 1</CoolButton>
-      <CoolButton>Button 2</CoolButton>
+      {/* <CoolButton isSuccess>Button 1</CoolButton>
+      <CoolButton>Button 2</CoolButton> */}
+      
+        <Message isInfo title="Hello World">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+                <strong>Pellentesque risus mi</strong>.
+        </Message>
+
+      <CoolButton isSmall isDanger isRounded>Button 1</CoolButton>
+      <CoolButton isSmall isSuccess>Button 2</CoolButton>
     </div>
   );
 }
